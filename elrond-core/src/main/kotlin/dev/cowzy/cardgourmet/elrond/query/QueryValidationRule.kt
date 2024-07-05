03609a833633fdebbaee05df329d92505a17b64a
Benedikt Wüller <me@benedikt.dev>
2024-07-05 18:14:48 +0200

    patch: add serial names to validation rules

--- elrond-core/src/main/kotlin/dev/cowzy/cardgourmet/elrond/query/QueryValidationRule.kt
+++ elrond-core/src/main/kotlin/dev/cowzy/cardgourmet/elrond/query/QueryValidationRule.kt
@@ -1,9 +1,13 @@
 package dev.cowzy.cardgourmet.elrond.query
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class QueryValidationRule {
-    NO_IGNORED_VALUES,
-    NO_CUSTOM_DISTINCT_MODE,
-    NO_CUSTOM_SORTING,
-    NO_CUSTOM_FLAGS,
-    NOT_EMPTY
+    @SerialName("no_ignored_values") NO_IGNORED_VALUES,
+    @SerialName("no_custom_distinct_mode") NO_CUSTOM_DISTINCT_MODE,
+    @SerialName("no_custom_sorting") NO_CUSTOM_SORTING,
+    @SerialName("no_custom_flags") NO_CUSTOM_FLAGS,
+    @SerialName("not_empty") NOT_EMPTY
 }
